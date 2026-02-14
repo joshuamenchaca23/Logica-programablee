15f7a91f1cc5b3f73ba45bd3d73d3254e90021d9
Joshua Ricardo Menchaca Ramirez <a01641943@tec.mx>
2026-02-14 12:03:10 -0600

    Add comments to primos.v for clarity
    Added comments explaining the prime number conditions and LED indication.

--- practica1/primos.v
+++ practica1/primos.v
@@ -1,8 +1,15 @@
+/* 
+En este código se declaran las condiciones para que los números primos del 0 al 15 
+tengan el valor de 1 (TRUE), de manera que se mande la indicación a la tarjeta 
+y al poner esos numeros se prenda el LED. Se usa default para indicar que todos los 
+otros números tengan el valor de 0. Es decir que no encienda el LED. 
+*/
+
 module primos( 
-	input[3:0] N,
+	input[3:0] N, //declaración del vector de 4 bits para los numeros
 	output reg out
 	);
-	always@(*) 
+	always@(*) //lista de sensivilidad
 	
 	begin 
 		case(N)
@@ -15,4 +22,5 @@ module primos(
 			default:out=0;
 			endcase 
 	end			
+
 endmodule 
